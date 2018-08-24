981a4ca9299a547101b4830126ef93746a17e896
Charles Bancroft <charlie.bancroft@gmail.com>
2018-08-24 13:05:55 -0400

    Update lets_split config to use new user config

--- keyboards/lets_split/keymaps/cbancroft/rules.mk
+++ keyboards/lets_split/keymaps/cbancroft/rules.mk
@@ -1,7 +1,7 @@
 ## BUILD OPTIONS
 MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
-NKRO_ENABLE = no           # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+NKRO_ENABLE = yes           # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 USE_I2C = yes
 TAP_DANCE_ENABLE = no
 
